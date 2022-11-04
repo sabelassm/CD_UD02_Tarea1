cadd5ddfa1d706226dda618234bc0042d4247e5d
Sabela <sabela@iessanclemente.net>
2022-11-04 13:33:18 +0100

    ejercicio·

---
+++ tareas/ejercicio1.java
@@ -0,0 +1,23 @@
+/*
+ * Signo del zodíaco
+ * Crea que al introducir el año que naciste te devuelva
+tu signo del zodíaco chino.
+Para conocer el signo del horóscopo chino, debemos
+dividir el año de nacimiento entre 12. El resto, entre
+0 y 11, está asociado a un signo según la siguiente
+tabla:
+
+0 -Mono
+1- Gallo
+2 -Perro
+2 - Cerdo
+4 -Rata 
+5- Buey 
+6 - Tigre
+7 - Conejo
+8 - Dragón
+9 -Serepiente
+10- Caballo
+11 - Cabra 
+
+ */
