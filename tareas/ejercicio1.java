36f4c6516cd0a60fb2c4cf6d02c4bdab95e94188
Gabriel <a22gabrielmv@iessanclemente.net>
2022-11-04 14:26:33 +0100

    añadido ej pt1

--- tareas/ejercicio1.java
+++ tareas/ejercicio1.java
@@ -1,23 +1,30 @@
-/*
- * Signo del zodíaco
- * Crea que al introducir el año que naciste te devuelva
-tu signo del zodíaco chino.
-Para conocer el signo del horóscopo chino, debemos
-dividir el año de nacimiento entre 12. El resto, entre
-0 y 11, está asociado a un signo según la siguiente
-tabla:
-
-0 -Mono
-1- Gallo
-2 -Perro
-2 - Cerdo
-4 -Rata 
-5- Buey 
-6 - Tigre
-7 - Conejo
-8 - Dragón
-9 -Serepiente
-10- Caballo
-11 - Cabra 
-
- */
+import java.util.Scanner;
+ 
+public class ejercicio1 {
+ 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce el año:");
+        int num = teclado.nextInt();
+        teclado.close();
+        String horoscopo = "";
+        int resto = num % 12;
+        switch (resto) {
+            case 0: horoscopo = "Mono"; break;
+            case 1: horoscopo = "Gallo"; break;
+            case 2: horoscopo = "Perro"; break;
+            case 3: horoscopo = "Cerdo"; break;
+            case 4: horoscopo = "Rata"; break;
+            case 5: horoscopo = "Buey"; break;
+            case 6: horoscopo = "Tigre"; break;
+            case 7: horoscopo = "Conejo"; break;
+            case 8: horoscopo = "Dragon"; break;
+            case 9: horoscopo = "Serpiente"; break;
+            case 10: horoscopo = "Caballo"; break;
+            case 11: horoscopo = "Cabra"; break;
+        }
+ 
+        System.out.println("Tu signo es: "+horoscopo);
+    }   
+ 
+}
--- tareas/ejercicio2.java
+++ tareas/ejercicio2.java
@@ -3,3 +3,12 @@
 Haz un script que reciba por parámetro al menos 4 nombres de persona y devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
 
  */
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+    
+        String[] nombres ={"Ana", "Carlos", "Jose", "Mason"};
+    
+    }
+}
