7affb70921bf0064cfbad03c864786278251209f
Gabriel <a22gabrielmv@iessanclemente.net>
2022-11-08 17:12:44 +0100

    Finalizado ej2

--- tareas/ejercicio2.java
+++ tareas/ejercicio2.java
@@ -1,14 +1,25 @@
-/*
- * 
-Haz un script que reciba por parámetro al menos 4 nombres de persona y devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
-
- */
-
-public class ejercicio2 {
-
-    public static void main(String[] args) {
-    
-        String[] nombres ={"Ana", "Carlos", "Jose", "Mason"};
-    
+public class ejercicio2
+{
+    public static void main(String[] args) 
+    {
+        if(args.length==0){
+            String[] nombres ={"Ana", "Carlos", "Jose", "Mason"};
+            imprimeNombres(nombres);
+        }
+        else{
+            imprimeNombres(args);
+        }
+    }
+    private static void imprimeNombres(String[] nombres)
+    {
+        System.out.print("Hola ");
+        
+        for(int i=0; i<nombres.length; i++) 
+        {
+            if(i>0)
+                System.out.print(", ");
+            System.out.print(nombres[i]);                
+        }
+        System.out.println("!");
     }
 }
