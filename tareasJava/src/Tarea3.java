fcb88a55c10e97bc5f04c3f87ee1e1140d9bd7fe
David Chaves Rodríguez <a22davidcr@iessanclemente.net>
2022-11-18 14:14:18 +0100

    Tarea3 corrixida

--- tareasJava/src/Tarea3.java
+++ tareasJava/src/Tarea3.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Tarea3{
+public class Tarea3 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Dinos a nota");
-        int nota= sc.nextInt();
+        int nota = sc.nextInt();
 
-        if (nota<5) {
-            System.out.println("Suspendeches, papón");
+        if (0 <= nota && nota <= 10) {
+            if (nota < 5) {
+                System.out.println("Suspendeches, papón");
+            } else {
+                System.out.println("Moi ben, rei");
+            }
+        } else {
+            System.out.println("Aprende a escribir");
         }
-        else{
-            System.out.println("Moi ben, rei");
-        }
+        sc.close();
     }
 }
