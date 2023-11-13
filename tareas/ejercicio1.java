9fd6136b50e2e31c4033df5a32959d87534c98e3
=Vanessa <=a23vanessabg@iessanclemente.net>
2023-11-13 11:08:09 +0100

    Ejercicios java

--- tareas/ejercicio1.java
+++ tareas/ejercicio1.java
@@ -21,3 +21,63 @@ tabla:
 11 - Cabra 
 
  */
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int anhoUsuario;
+        
+        System.out.println("Voy a decirte tu signo en el horóscopo chino");
+        System.out.println("¿En qué año naciste? ");
+        anhoUsuario = teclado.nextInt();
+        int resto = anhoUsuario%12;
+
+        switch (resto) {
+            case 0:
+                System.out.println("Tu signo es Mono");
+                break;
+            case 1:
+                System.out.println("Tu signo es Gallo");
+                break;
+            case 2:
+                System.out.println("Tu signo es Perro");
+                break;
+            case 3:
+                System.out.println("Tu signo es Cerdo");
+                break;
+            case 4:
+                System.out.println("Tu signo es Rata");
+                break;
+            case 5:
+                System.out.println("Tu signo es Buey");
+                break;
+            case 6:
+                System.out.println("Tu signo es Tigre");
+                break;
+            case 7:
+                System.out.println("Tu signo es Conejo");
+                break;
+            case 8:
+                System.out.println("Tu signo es Dragón");
+                break;
+            case 9:
+                System.out.println("Tu signo es Serpiente");
+                break;
+            case 10:
+                System.out.println("Tu signo es Caballo");
+                break;
+            case 11:
+                System.out.println("Tu signo es Cabra");
+                break;
+            default:
+            System.out.println("No has introducido un número correcto");
+                break;
+        }
+
+    teclado.close();
+    }
+}
--- tareas/ejercicio2.java
+++ tareas/ejercicio2.java
@@ -3,3 +3,28 @@
 Haz un script que reciba por parámetro al menos 4 nombres de persona y devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
 
  */
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String nombre1;
+        String nombre2;
+        String nombre3;
+        String nombre4;
+
+        System.out.println("Permitidme que os salude correctamente:");
+        
+        System.out.println("Dime un nombre: ");
+        nombre1= teclado.nextLine();
+        nombre2= teclado.nextLine();
+        nombre3= teclado.nextLine();
+        nombre4= teclado.nextLine();
+        System.out.println("¡Hola "+nombre1+", "+nombre2+", "+nombre3+", "+nombre4+"!");
+        
+        teclado.close();
+    }
+}
