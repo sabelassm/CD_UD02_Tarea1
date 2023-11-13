b856cc93f79504e50d4c9f40ece8cc02873d66c5
daw1FPD - Quintela Babío <A23ElenaQB@sanclemente.local>
2023-11-13 10:58:40 +0100

    Exercicio 2 resolto

--- tareas/ejercicio2.java
+++ tareas/ejercicio2.java
@@ -3,3 +3,30 @@
 Haz un script que reciba por parámetro al menos 4 nombres de persona y devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
 
  */
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+        String[] nomes = new String[4];
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.print("Introduce o nome da persoa " + (i + 1) + ": ");
+            nomes[i] = escaner.nextLine();
+        }
+        String saudo = "Ola ";
+        for (int i = 0; i < nomes.length; i++) {
+            saudo += nomes[i];
+            if (i < nomes.length - 1) {
+                saudo += ", ";
+            }
+        }
+        saudo += "!";
+        System.out.println(saudo);
+
+        escaner.close();
+
+    }
+ 
+    
+ }
