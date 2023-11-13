2cbcb58b8dce6af867df7fe42d9b09cc4504d266
Carolina <a23carolinabc@iessanclemente.net>
2023-11-13 11:10:52 +0100

    Ejercicio 1 realizado

--- tareas/ejercicio1.java
+++ tareas/ejercicio1.java
@@ -16,8 +16,63 @@ tabla:
 6 - Tigre
 7 - Conejo
 8 - Dragón
-9 -Serepiente
+9 -Serpiente
 10- Caballo
 11 - Cabra 
 
  */
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce año de nacimiento");
+        int anho = scanner.nextInt();
+        int signo = anho%12;
+        switch (signo) {
+            case 0:
+                System.out.println("Año del mono");
+                break;
+            case 1:
+                System.out.println("Año del gallo");
+                break;
+            case 2:
+                System.out.println("Año del perro");
+                break;
+            case 3:
+                System.out.println("Año del cerdo");
+                break;
+            case 4:
+                System.out.println("Año del rata");
+                break;
+            case 5:
+                System.out.println("Año del buey");
+                break;
+            case 6:
+                System.out.println("Año del tigre");
+                break;
+            case 7:
+                System.out.println("Año del conejo");
+                break;
+            case 8:
+                System.out.println("Año del dragón");
+                break;
+            case 9:
+                System.out.println("Año del serpiente");
+                break;
+            case 10:
+                System.out.println("Año del caballo");
+                break;
+            case 11:
+                System.out.println("Año del cabra");
+                break;
+            default:
+                break;
+        }
+
+
+        scanner.close();
+    }
+}
