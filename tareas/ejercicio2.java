05e0875147781aaeceee2a8f126737fa1b4840d2
Carolina <a23carolinabc@iessanclemente.net>
2023-11-13 11:27:30 +0100

    Realizado ejercicio 2

--- tareas/ejercicio2.java
+++ tareas/ejercicio2.java
@@ -3,3 +3,19 @@
 Haz un script que reciba por parámetro al menos 4 nombres de persona y devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
 
  */
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] nombre = new String[4];
+        System.out.println("Introduce cuatro nombres");
+        for (int i = 0; i < nombre.length; i++) {
+            nombre[i]= scanner.nextLine();
+        }
+        System.out.println("¡Hola "+nombre[0]+","+nombre[1]+","+nombre[2]+","+nombre[3]+"!");
+        scanner.close();
+    }
+}
