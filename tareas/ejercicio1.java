2b76da2f2e660c652c60854c41b4bfbc1f2365f5
daw1FPD - Quintela Babío <A23ElenaQB@sanclemente.local>
2023-11-13 10:44:24 +0100

    Exercicio 1 resolto

--- tareas/ejercicio1.java
+++ tareas/ejercicio1.java
@@ -10,14 +10,72 @@ tabla:
 0 -Mono
 1- Gallo
 2 -Perro
-2 - Cerdo
+3 - Cerdo
 4 -Rata 
 5- Buey 
 6 - Tigre
 7 - Conejo
 8 - Dragón
-9 -Serepiente
+9 -Serpiente
 10- Caballo
 11 - Cabra 
 
  */
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("En que ano naciches?");
+        int ano = escaner.nextInt();
+        int signo = ano % 12;
+
+        if (signo >= 0 && signo <= 11) {
+            switch (signo) {
+                case 0:
+                    System.out.println("Mono");
+                    break;
+                case 1:
+                    System.out.println("Gallo");
+                    break;
+                case 2:
+                    System.out.println("Perro");
+                    break;
+                case 3:
+                    System.out.println("Cerdo");
+                    break;
+                case 4:
+                    System.out.println("Rata");
+                    break;
+                case 5:
+                    System.out.println("Buey");
+                    break;
+                case 6:
+                    System.out.println("Tigre");
+                    break;
+                case 7:
+                    System.out.println("Conejo");
+                    break;
+                case 8:
+                    System.out.println("Dragón");
+                    break;
+                case 9:
+                    System.out.println("Serpiente");
+                    break;
+                case 10:
+                    System.out.println("Caballo");
+                    break;
+
+                case 11:
+                    System.out.println("Cabra");
+                    break;
+            }
+        }
+
+        escaner.close();
+
+    }
+
+}
